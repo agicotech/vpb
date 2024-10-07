93c1d3a547fd1786029734318db8e3c0a68e8c32
simplycleverlol <akgeorg2101@yandex.ru>
2024-10-07 21:29:48 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 add-apt-repository ppa:deadsnakes/ppa -y
 apt update
 apt upgrade -y
-apt install git -y
+
+apt install git curl software-properties-common ca-certificates apt-transport-https -y
+wget -O- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable"| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce -y
+
 
 git clone https://github.com/simplycleverlol/vpb.git
 

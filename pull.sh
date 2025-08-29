b76170cc65b661d6f49a9bc0a45ccc8d0e13c976
agicotech <agicotech@yandex.com>
2025-08-29 14:18:24 +0300

    service reinstall on startup

--- pull.sh
+++ pull.sh
@@ -1,4 +1,6 @@
 cd /root/vpb
 git pull
 ./venv/bin/python -m pip install -r requirements.txt
+cp /root/vpb/vpn_api.service /lib/systemd/system/vpn_api.service
+systemctl daemon-reload
 service vpn_api restart
